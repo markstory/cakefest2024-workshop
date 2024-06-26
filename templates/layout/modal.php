96040143804247b4ccaf8cf073af71ea2c97f4c3
Mark Story <mark@mark-story.com>
2024-06-26 00:58:54 -0400

    Update modal window to a webcomponent and use esm modules
    using esm modules makes small javascript projects easier to organize.
    Pages can include additional components as required.

--- templates/layout/modal.php
+++ templates/layout/modal.php
@@ -1,18 +1,8 @@
 <?php
 declare(strict_types=1);
 ?>
-<dialog id="modal-window">
-    <?= $this->fetch('content') ?>
-</dialog>
-<script type="text/javascript">
-(function() {
-    const modal = document.querySelector('#modal-window');
-    modal.showModal();
-    modal
-        .querySelector('[modal-close="1"]')
-        .addEventListener('click', function (event) {
-            event.preventDefault();
-            modal.close();
-        }, false);
-}());
-</script>
+<modal-window>
+    <dialog id="modal-window">
+        <?= $this->fetch('content') ?>
+    </dialog>
+</modal-window>
