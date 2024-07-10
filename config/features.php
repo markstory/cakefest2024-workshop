70c854c942e58bdc3f7fff6ce8b864ec95bc4cbd
Mark Story <mark@mark-story.com>
2024-07-10 00:24:42 -0400

    Make feature check better

--- config/features.php
+++ config/features.php
@@ -6,6 +6,22 @@ return [
         'organizations:vip' => [
             'segments' => [
                 [
+                    'name' => 'internal test',
+                    'conditions' => [
+                        [
+                            'property' => 'organization_slug',
+                            'op' => 'in',
+                            'value' => ['cakephp'],
+                        ],
+                        [
+                            'property' => 'user_email',
+                            'op' => 'in',
+                            'value' => ['owner@example.com'],
+                        ],
+                    ],
+                    'rollout' => 100,
+                ],
+                [
                     'name' => 'vip',
                     'conditions' => [
                         [
